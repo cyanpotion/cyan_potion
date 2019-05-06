eb95fdbdd1ae57a436a3bb77f78208c2208bfbc9
XenoAmess <xenoamess@gmail.com>
2019-05-06 14:51:34 +0800

    Revert "..."
    This reverts commit 56a76c92a493f13e20e7f1b01cf098087739098f.

--- src/base/src/main/java/com/xenoamess/cyan_potion/base/io/FileUtil.java
+++ src/base/src/main/java/com/xenoamess/cyan_potion/base/io/FileUtil.java
@@ -25,7 +25,7 @@
 package com.xenoamess.cyan_potion.base.io;
 
 import org.lwjgl.BufferUtils;
-import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.MemoryStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -78,20 +78,19 @@ public class FileUtil {
      * @return the resource data
      */
     public static ByteBuffer loadFileBuffer(File resourceFile) {
-        return loadFileBuffer(resourceFile, false);
+        return loadFileBuffer(null, resourceFile);
     }
 
 
     /**
      * Reads the specified resource and returns the raw data as a ByteBuffer.
-     * if ifUsingMemoryUtil == false, then use BufferUtil.
-     * else, allocate it using MemoryUtil.
+     * if memoryStack == null, then use BufferUtil.
+     * else, allocate it from memoryStack.
      *
-     * @param resourceFile      the resource file to read
-     * @param ifUsingMemoryUtil if using MemoryUtil here
+     * @param resourceFile the resource file to read
      * @return the resource data
      */
-    public static ByteBuffer loadFileBuffer(File resourceFile, boolean ifUsingMemoryUtil) {
+    public static ByteBuffer loadFileBuffer(MemoryStack memoryStack, File resourceFile) {
         boolean success;
 
         ByteBuffer buffer = null;
@@ -99,10 +98,10 @@ public class FileUtil {
         Path path = Paths.get(absolutePath);
         if (Files.isReadable(path)) {
             try (SeekableByteChannel fc = Files.newByteChannel(path)) {
-                if (ifUsingMemoryUtil) {
-                    buffer = MemoryUtil.memAlloc((int) fc.size() + 1);
-                } else {
+                if (memoryStack == null) {
                     buffer = BufferUtils.createByteBuffer((int) fc.size() + 1);
+                } else {
+                    buffer = memoryStack.malloc((int) fc.size() + 1);
                 }
 
 
@@ -129,10 +128,10 @@ public class FileUtil {
                 ReadableByteChannel rbc = Channels.newChannel(source)
         ) {
 
-            if (ifUsingMemoryUtil) {
-                buffer = MemoryUtil.memAlloc((int) resourceFile.length() + 1);
-            } else {
+            if (memoryStack == null) {
                 buffer = BufferUtils.createByteBuffer((int) resourceFile.length() + 1);
+            } else {
+                buffer = memoryStack.malloc((int) resourceFile.length() + 1);
             }
 
             while (true) {
