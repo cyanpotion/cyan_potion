5ed65328c6f2b1bb23132f15a3550a9f77dabc47
XenoAmess <xenoamess@gmail.com>
2019-05-16 14:50:33 +0800

    update jackson to 2.9.9

--- deploy.cmd
+++ deploy.cmd
@@ -2,13 +2,13 @@ call mvn clean deploy -Dmaven.javadoc.skip=false
 cd ./src/
 cd ./base/
 call mvn clean deploy -Dmaven.javadoc.skip=falsecd 
-../
+cd ../
 cd ./coordinate
 call mvn clean deploy -Dmaven.javadoc.skip=falsecd 
-../
+cd ../
 cd ./rpg_module
 call mvn clean deploy -Dmaven.javadoc.skip=falsecd 
-../
+cd ../
 cd ./demo
 call mvn clean deploy -Dmaven.javadoc.skip=false
 pause
