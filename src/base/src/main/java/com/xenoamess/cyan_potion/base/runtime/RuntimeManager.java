3311b590c5b1eaf961a0ee3038dfb3abac6d3b6e
XenoAmess <xenoamess@gmail.com>
2020-01-23 05:39:05 +0800

    fix empty event mechanism,version to 0.148.0, prepare for lunar new year.

--- src/base/src/main/java/com/xenoamess/cyan_potion/base/runtime/RuntimeManager.java
+++ src/base/src/main/java/com/xenoamess/cyan_potion/base/runtime/RuntimeManager.java
@@ -1,45 +1,46 @@
-///*
-// * MIT License
-// *
-// * Copyright (c) 2020 XenoAmess
-// *
-// * Permission is hereby granted, free of charge, to any person obtaining a copy
-// * of this software and associated documentation files (the "Software"), to deal
-// * in the Software without restriction, including without limitation the rights
-// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// * copies of the Software, and to permit persons to whom the Software is
-// * furnished to do so, subject to the following conditions:
-// *
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// *
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// * SOFTWARE.
-// */
-//
-//package com.xenoamess.cyan_potion.base.runtime;
-//
-//import com.xenoamess.cyan_potion.base.GameManager;
-//
-//public class RuntimeManager {
-//    private final GameManager gameManager;
-//
-//    /**
-//     * <p>Constructor for DataCenter.</p>
-//     *
-//     * @param gameManager gameManager
-//     */
-//    public RuntimeManager(GameManager gameManager) {
-//        this.gameManager = gameManager;
-//    }
-//
-//    public GameManager getGameManager() {
-//        return gameManager;
-//    }
-//
-//}
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 XenoAmess
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.xenoamess.cyan_potion.base.runtime;
+
+import com.xenoamess.cyan_potion.base.GameManager;
+
+//TODO
+public class RuntimeManager {
+    private final GameManager gameManager;
+
+    /**
+     * <p>Constructor for DataCenter.</p>
+     *
+     * @param gameManager gameManager
+     */
+    public RuntimeManager(GameManager gameManager) {
+        this.gameManager = gameManager;
+    }
+
+    public GameManager getGameManager() {
+        return gameManager;
+    }
+
+}
