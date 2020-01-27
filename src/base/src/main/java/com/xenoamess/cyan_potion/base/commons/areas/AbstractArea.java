f013abf0095b20dd49e7e160e85f0288aa0d5f8d
XenoAmess <xenoamess@gmail.com>
2020-01-27 16:29:02 +0800

    add documents for AbstractArea. and add missing functions getRightCenterPosX and getRightCenterPosY. Godammit how can I miss them...

--- src/base/src/main/java/com/xenoamess/cyan_potion/base/commons/areas/AbstractArea.java
+++ src/base/src/main/java/com/xenoamess/cyan_potion/base/commons/areas/AbstractArea.java
@@ -25,7 +25,11 @@
 package com.xenoamess.cyan_potion.base.commons.areas;
 
 /**
- * <p>Area interface.</p>
+ * AbstractArea means a rectangle(or maybe not rectangle, but something else like circle)
+ * who have a position,and width, and height.
+ * by default(of this engine), the area must be totally contained in the described rectangle.
+ * for example, if the shape is a circle, then the area must be the circumscribed rectangle of the circle.
+ * you can see implement of this class for more details.
  *
  * @author XenoAmess
  * @version 0.143.0
@@ -39,110 +43,218 @@ public interface AbstractArea {
     boolean ifMutable();
 
     /**
-     * <p>getLeftTopPosX.</p>
+     * get left top posX of this area
      *
      * @return a float.
      */
     float getLeftTopPosX();
 
     /**
-     * <p>getLeftTopPosY.</p>
+     * get left top posY of this area
      *
      * @return a float.
      */
     float getLeftTopPosY();
 
+    /**
+     * get center posX of this area
+     *
+     * @return a float.
+     */
     default float getCenterPosX() {
         return this.getLeftTopPosX() + this.getWidth() / 2F;
     }
 
+    /**
+     * get center posY of this area
+     *
+     * @return a float.
+     */
     default float getCenterPosY() {
         return this.getLeftTopPosY() + this.getHeight() / 2F;
     }
 
 
+    /**
+     * get left posX of this area
+     *
+     * @return a float.
+     */
     default float getLeftPosX() {
         return this.getLeftTopPosX();
     }
 
+    /**
+     * get right posX of this area
+     *
+     * @return a float.
+     */
     default float getRightPosX() {
         return this.getLeftPosX() + this.getWidth();
     }
 
+    /**
+     * get top posY of this area
+     *
+     * @return a float.
+     */
     default float getTopPosY() {
         return this.getLeftTopPosY();
     }
 
+    /**
+     * get bottom posY of this area
+     *
+     * @return a float.
+     */
     default float getBottomPosY() {
         return this.getTopPosY() + this.getHeight();
     }
 
 
+    /**
+     * get left bottom posX of this area
+     *
+     * @return a float.
+     */
     default float getLeftBottomPosX() {
         return this.getLeftPosX();
     }
 
+    /**
+     * get left bottom posY of this area
+     *
+     * @return a float.
+     */
     default float getLeftBottomPosY() {
         return this.getBottomPosY();
     }
 
+    /**
+     * get right top posX of this area
+     *
+     * @return a float.
+     */
     default float getRightTopPosX() {
         return this.getRightPosX();
     }
 
+    /**
+     * get right top posY of this area
+     *
+     * @return a float.
+     */
     default float getRightTopPosY() {
         return this.getTopPosY();
     }
 
+    /**
+     * get right bottom posX of this area
+     *
+     * @return a float.
+     */
     default float getRightBottomPosX() {
         return this.getRightPosX();
     }
 
+    /**
+     * get right bottom posY of this area
+     *
+     * @return a float.
+     */
     default float getRightBottomPosY() {
         return this.getBottomPosY();
     }
 
+    /**
+     * get left center posX of this area
+     *
+     * @return a float.
+     */
     default float getLeftCenterPosX() {
         return this.getLeftPosX();
     }
 
+    /**
+     * get left center posY of this area
+     *
+     * @return a float.
+     */
     default float getLeftCenterPosY() {
         return this.getCenterPosY();
     }
 
+    /**
+     * get right center posX of this area
+     *
+     * @return a float.
+     */
+    default float getRightCenterPosX() {
+        return this.getLeftPosX();
+    }
+
+    /**
+     * get right center posY of this area
+     *
+     * @return a float.
+     */
+    default float getRightCenterPosY() {
+        return this.getCenterPosY();
+    }
+
+    /**
+     * get center top posX of this area
+     *
+     * @return a float.
+     */
     default float getCenterTopPosX() {
         return this.getCenterPosX();
     }
 
+    /**
+     * get center top posY of this area
+     *
+     * @return a float.
+     */
     default float getCenterTopPosY() {
         return this.getTopPosY();
     }
 
+    /**
+     * get center bottom posX of this area
+     *
+     * @return a float.
+     */
     default float getCenterBottomPosX() {
         return this.getCenterPosX();
     }
 
+    /**
+     * get center bottom posY of this area
+     *
+     * @return a float.
+     */
     default float getCenterBottomPosY() {
         return this.getBottomPosY();
     }
 
 
     /**
-     * <p>getWidth.</p>
+     * get width of this area
      *
-     * @return a float.
+     * @return width of this area.
      */
     float getWidth();
 
     /**
-     * <p>getHeight.</p>
+     * get height of this area
      *
-     * @return a float.
+     * @return height of this area.
      */
     float getHeight();
 
     /**
-     * <p>copyArea.</p>
+     * get a SimpleImmutableArea build from this area.
      *
      * @return return
      */
