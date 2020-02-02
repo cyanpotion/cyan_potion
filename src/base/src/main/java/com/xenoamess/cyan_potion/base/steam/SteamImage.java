77b18fdeb069ca7c0a701e7bca60dccb13d7a67e
XenoAmess <xenoamess@gmail.com>
2020-02-02 15:22:58 +0800

    kill tabs

--- src/base/src/main/java/com/xenoamess/cyan_potion/base/steam/SteamImage.java
+++ src/base/src/main/java/com/xenoamess/cyan_potion/base/steam/SteamImage.java
@@ -38,49 +38,49 @@ import java.nio.ByteBuffer;
  * But right now let's just use this class here.
  */
 public class SteamImage {
-	private final int imageHandle;
-	private int width = -1;
-	private int height = -1;
-	private ByteBuffer imageBuffer = null;
+    private final int imageHandle;
+    private int width = -1;
+    private int height = -1;
+    private ByteBuffer imageBuffer = null;
 
-	SteamImage(int imageHandle) {
-		this.imageHandle = imageHandle;
-	}
+    SteamImage(int imageHandle) {
+        this.imageHandle = imageHandle;
+    }
 
-	public int getImageHandle() {
-		return imageHandle;
-	}
+    public int getImageHandle() {
+        return imageHandle;
+    }
 
-	public int getWidth(SteamUtils steamUtils) {
-		if (width == -1) {
-			this.getImageSizeAndSet(steamUtils);
-		}
-		return width;
-	}
+    public int getWidth(SteamUtils steamUtils) {
+        if (width == -1) {
+            this.getImageSizeAndSet(steamUtils);
+        }
+        return width;
+    }
 
-	public int getHeight(SteamUtils steamUtils) {
-		if (height == -1) {
-			this.getImageSizeAndSet(steamUtils);
-		}
-		return height;
-	}
+    public int getHeight(SteamUtils steamUtils) {
+        if (height == -1) {
+            this.getImageSizeAndSet(steamUtils);
+        }
+        return height;
+    }
 
-	public ByteBuffer getImageBuffer(SteamUtils steamUtils) throws SteamException {
-		if (this.imageBuffer != null) {
-			return this.imageBuffer;
-		}
-		ByteBuffer imageBuffer = ByteBuffer.allocateDirect(this.getWidth(steamUtils) * this.getHeight(steamUtils) * 4);
-		if (steamUtils.getImageRGBA(getImageHandle(), imageBuffer)) {
-			this.imageBuffer = imageBuffer;
-		}
-		return this.imageBuffer;
-	}
+    public ByteBuffer getImageBuffer(SteamUtils steamUtils) throws SteamException {
+        if (this.imageBuffer != null) {
+            return this.imageBuffer;
+        }
+        ByteBuffer imageBuffer = ByteBuffer.allocateDirect(this.getWidth(steamUtils) * this.getHeight(steamUtils) * 4);
+        if (steamUtils.getImageRGBA(getImageHandle(), imageBuffer)) {
+            this.imageBuffer = imageBuffer;
+        }
+        return this.imageBuffer;
+    }
 
-	protected void getImageSizeAndSet(SteamUtils steamUtils) {
-		int[] size = new int[2];
-		steamUtils.getImageSize(getImageHandle(), size);
-		this.width = size[0];
-		this.height = size[1];
-	}
+    protected void getImageSizeAndSet(SteamUtils steamUtils) {
+        int[] size = new int[2];
+        steamUtils.getImageSize(getImageHandle(), size);
+        this.width = size[0];
+        this.height = size[1];
+    }
 
 }
