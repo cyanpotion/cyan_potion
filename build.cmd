5ebe7a89d1376c0cdbd0f41c00cd0310fbbdb4a0
XenoAmess <xenoamess@gmail.com>
2020-08-21 20:00:22 +0800

    version to 0.163.0-SNAPSHOT greatly changed maven project structure.

--- build.cmd
+++ build.cmd
@@ -1,14 +1,1 @@
 call mvn clean install -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ./src/
-cd ./base/
-call mvn clean install -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./coordinate
-call mvn clean install -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./rpg_module
-call mvn clean install -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./demo
-call mvn clean install -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-pause
--- clean.cmd
+++ clean.cmd
@@ -1,14 +1,1 @@
 call mvn clean
-cd ./src/
-cd ./base/
-call mvn clean
-cd ../
-cd ./coordinate
-call mvn clean
-cd ../
-cd ./rpg_module
-call mvn clean
-cd ../
-cd ./demo
-call mvn clean
-pause
