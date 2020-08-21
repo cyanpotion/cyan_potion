7a1942810ba8dc0d2b9c29793aedd5a1b377a866
XenoAmess <xenoamess@gmail.com>
2020-08-21 20:47:00 +0800

    add new lines at files ends

--- site.cmd
+++ site.cmd
@@ -1,14 +1,1 @@
 call mvn clean site -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ./src/
-cd ./base/
-call mvn clean site -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./coordinate
-call mvn clean site -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./rpg_module
-call mvn clean site -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-cd ../
-cd ./demo
-call mvn clean site -Dmaven.javadoc.skip=false -Pauto_clean_delombok
-pause
