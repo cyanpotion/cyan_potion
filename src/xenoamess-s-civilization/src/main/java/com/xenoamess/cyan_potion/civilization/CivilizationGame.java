f35dbe93361c9440026ab2e8bbe5b8f87c2f099d
XenoAmess <xenoamess@gmail.com>
2026-03-01 18:40:04 +0000

    feat: add xenoamess-s-civilization module
    - Create new Maven module: xenoamess-s-civilization
    - Add module to root pom.xml
    - Include dependency on base, coordinate, rpg_module
    - Add entry point class CivilizationGame

---
+++ src/xenoamess-s-civilization/src/main/java/com/xenoamess/cyan_potion/civilization/CivilizationGame.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (C) 2020 XenoAmess
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.xenoamess.cyan_potion.civilization;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * XenoAmess's Civilization module entry point.
+ *
+ * @author XenoAmess
+ * @version 0.167.3-SNAPSHOT
+ */
+@Slf4j
+public class CivilizationGame {
+
+    /**
+     * Main entry point for the civilization game.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        LOGGER.info("Welcome to XenoAmess's Civilization!");
+    }
+}
